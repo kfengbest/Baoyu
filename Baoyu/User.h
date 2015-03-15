aecfb21ea0a102dd80f6ce3580c703af963a3018
= <kaven.feng@autodesk.com>
2015-03-15 14:05:10 +0800

    create user as placeholder

---
+++ Baoyu/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  Baoyu
+//
+//  Created by Kaven Feng on 3/15/15.
+//  Copyright (c) 2015 Kaven Feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ Baoyu/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Baoyu
+//
+//  Created by Kaven Feng on 3/15/15.
+//  Copyright (c) 2015 Kaven Feng. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
